e130e0d901a8dde462697906cca1c71fe6a63ea1
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-05-30 15:50:49 +0700

    ADD - Ant Design Color

---
+++ src/layouts/MainLayout/Header/Header.tsx
@@ -0,0 +1,13 @@
+const PRIMARY_COLOR: string = '#36CFC9';
+
+export const theme = {
+    dark: '',
+    light: '',
+    colors: {
+        primary: PRIMARY_COLOR,
+        
+    },
+    transition: {
+        primary: 'all 0.25s linear'
+    }
+}
